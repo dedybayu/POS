201454b08eacce3dda3c618275751d9934334d57
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-03-04 18:13:42 +0700

    add: Jobsheet2-Prak2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,21 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        $user = UserModel::FirstWhere('level_id', 1);
         return view('user', ['data' => $user]);
+
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
     }
 }
