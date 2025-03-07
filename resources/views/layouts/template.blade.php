25238511fca7bb0ae9fb722a8ffb452177762f60
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-03-07 22:54:08 +0700

    update layout

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -26,7 +26,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{url('/')}}" class="brand-link">
-                <img src="{{asset('adminlte/dist/iAmg/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+                <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
@@ -54,11 +54,11 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="{{ asset('adminlteplugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('adminlteplugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('adminltedist/js/adminlte.min.js')}}"></script>
+    <script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
 </body>
 
 </html>
