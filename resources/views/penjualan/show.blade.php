2edf39338da6564782851caebba5ff1e5bf1cef8
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-04-20 16:10:19 +0700

    add: detail penjualan

--- resources/views/penjualan/show.blade.php
+++ resources/views/penjualan/show.blade.php
@@ -18,26 +18,60 @@
     </div>
 @else
     <div class="modal-header">
-        <h5 class="modal-title">Data penjualan</h5>
+        <h5 class="modal-title">Data Penjualan</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
-    <div class="modal-body">
+    <div class="modal-body overflow-auto" style="max-height: 70vh;">
         <table class="table table-sm table-bordered table-striped">
             <tr>
-                <th class="text-right col-3">Nama penjualan :</th>
+                <th class="text-right col-3">Nama Pembeli :</th>
                 <td class="col-9">{{ $penjualan->pembeli }}</td>
             </tr>
             <tr>
-                <th class="text-right col-3">Kode penjualan :</th>
+                <th class="text-right col-3">Kode Penjualan :</th>
                 <td class="col-9">{{ $penjualan->penjualan_kode }}</td>
             </tr>
             <tr>
-                <th class="text-right col-3">Kode penjualan :</th>
-                <td class="col-9">{{ $penjualan->penjualan_kode }}</td>
+                <th class="text-right col-3">Total Harga :</th>
+                <td class="col-9">Rp{{ $total_harga }},00</td>
+            </tr>
+            <tr>
+                <th class="text-right col-3">Diproses Oleh :</th>
+                <td class="col-9">{{ $penjualan->user->nama . ' (' . $penjualan->user->level->level_kode . ')' }}</td>
             </tr>
         </table>
+        <h5 class="modal-title">Detail penjualan</h5>
+        <table class="table table-bordered mt-3">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Barang</th>
+                    <th>Harga Satuan</th>
+                    <th>Jumlah</th>
+                    <th>Subtotal</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($penjualan->penjualan_detail as $i => $detail)
+                <tr>
+                    <td>{{ $i + 1 }}</td>
+                    <td>{{ $detail->barang->barang_nama ?? '-' }}</td>
+                    <td>Rp{{ number_format($detail->harga, 0, ',', '.') }},00</td>
+                    <td>{{ $detail->jumlah }}</td>
+                    <td>Rp{{ number_format($detail->harga * $detail->jumlah, 0, ',', '.') }},00</td>
+                </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="4" class="text-end">Total</th>
+                    <th>Rp{{ $total_harga }},00</th>
+                </tr>
+            </tfoot>
+        </table>
+        
     </div>
     <div class="modal-footer">
         <button onclick="modalAction('{{ url('/penjualan/' . $penjualan->penjualan_id . '/edit_ajax') }}')" 
